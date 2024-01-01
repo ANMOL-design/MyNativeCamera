1ca0671993e743156f8e95f2ede64db2cb28f3db
Harshit Bhalla <harshitbhalla@Harshits-Mac-mini.local>
2024-01-01 19:08:14 +0530

    Added the local storage using sqlite

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,16 +1,87 @@
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_location_picker/models/place.dart';
+// Get sqlite package to store the data
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+// Get package Get Image Path and Save it
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+
+Future<Database> _getDatebase() async {
+  // Making the request to save data on device
+  final dbPath = await sql.getDatabasesPath();
+  // NOTE: The path below is import from library
+  // The below command opens the dbPath is exist or else created if not
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+    version: 1,
+  );
+
+  return db;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
+  Future<void> loadPlaces() async {
+    final db = await _getDatebase();
+    // Make a db query and fetch the data
+    final data = await db.query('user_places ');
+
+    final places = data
+        .map(
+          (item) => Place(
+            id: item['id'] as String,
+            title: item['title'] as String,
+            image: File(item['image'] as String),
+            location: PlaceLocation(
+                latitiude: item['lat'] as double,
+                longitude: item['lng'] as double,
+                address: item['address'] as String),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
+
+  void addPlace(String title, File image, PlaceLocation location) async {
+    // Saving the Images & other data to a Location using Path Provider Package
+    // Get the Directory Location
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+
+    // Get the file path
+    final filename = path.basename(image.path);
+
+    // print('${appDir.path}/$filename');
+
+    final copiedImage = await image
+        .copy('${appDir.path}/$filename'); // File method to save image
+
     final newPlace = Place(
       title: title,
-      image: image,
+      image: copiedImage,
       location: location,
     );
+
+    final db = await _getDatebase();
+
+    // Inserting the data to db
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitiude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
     state = [newPlace, ...state];
   }
 }
--- lib/screens/place.dart
+++ lib/screens/place.dart
@@ -4,11 +4,26 @@ import 'package:image_location_picker/providers/user_places.dart';
 import 'package:image_location_picker/screens/add_place.dart';
 import 'package:image_location_picker/widgets/places_list.dart';
 
-class PlaceScreen extends ConsumerWidget {
+class PlaceScreen extends ConsumerStatefulWidget {
   const PlaceScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlaceScreenState();
+  }
+}
+
+class _PlaceScreenState extends ConsumerState<PlaceScreen> {
+  late Future<void> _placeFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placeFuture = ref.read(userPlaceProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // Fetch Places from Riverpod
     final userPlaces = ref.watch(userPlaceProvider);
 
@@ -29,11 +44,18 @@ class PlaceScreen extends ConsumerWidget {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: PlaceList(
-          places: userPlaces,
-        ),
-      ),
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: _placeFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PlaceList(
+                        places: userPlaces,
+                      ),
+          )),
     );
   }
 }
